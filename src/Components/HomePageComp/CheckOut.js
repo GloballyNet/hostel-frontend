71157447c66431ef6943619fa8395a7e38e21613
Deepak <deepaksaraswat238@gmail.com>
2024-02-14 09:04:00 +0530

    Homepage updated

---
+++ src/Components/HomePageComp/CheckOut.js
@@ -0,0 +1,10 @@
+
+
+export const CheckOut = () => {
+  return (
+    <div className="text-white">
+        <h1 className="font-bold text-5xl mb-[1%]">Find your next stay</h1>
+        <p className="text-xl pb-[5%]">Search low prices on hostels, night stays and much more...</p>
+    </div>
+  )
+}
